a9c19c9b03fd3066fe619eca27e1f045df9dc2e9
qmliu <qmliu@pearlinpalm.com>
2021-06-09 12:39:45 +0800

    setting app them

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "Car Rental",
       initialRoute: AppPages.INITIAL,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.mulishTextTheme(),
+      ),
+      debugShowCheckedModeBanner: false,
+      defaultTransition: Transition.cupertino,
+      opaqueRoute: Get.isOpaqueRouteDefault,
+      popGesture: Get.isPopGestureEnable,
       getPages: AppPages.routes,
     ),
   );
