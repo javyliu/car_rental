f3ce419ba1d5ae50bca01cafe80224ded495a42a
qmliu <qmliu@pearlinpalm.com>
2021-06-09 13:44:23 +0800

    add assets

---
+++ lib/app/config.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class Config {
+  static const Color primaryColor = Color.fromRGBO(3, 120, 221, 1);
+  static const Color primaryColorShadow = Color.fromRGBO(3, 120, 221, .2);
+}
